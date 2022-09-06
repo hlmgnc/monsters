44c2cdd376359bdd97b9572086fba1e6f160d303
hlmgnc <61838067+hlmgnc@users.noreply.github.com>
2022-09-06 12:58:17 +0200

    Create card component and style files

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+import "./card.styles.css";
+
+class Card extends Component {
+  render() {
+    const { monster } = this.props;
+    const { id, name, email } = monster;
+
+    return (
+      <div className="card-container">
+        <img
+          alt={`monster ${name}`}
+          src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
